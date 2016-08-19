29791d3977bebeab16880bbec9369bb938fad241
Matt Brock <mbrock8@gmail.com>
2016-08-19 17:06:57 -0500

    add js in to site and build script to add active class to live page

--- js/site.js
+++ js/site.js
@@ -1,1 +1,15 @@
-// testing
+
+
+$(document).ready(function(){
+	
+	var pagePath = window.location.pathname;
+	
+	$('.nav__item').each(function(){
+		var h = $(this).find('a').attr('href').split('/')[1];
+		if(pagePath.search(h) > -1){
+			$(this).addClass('nav__item--active');
+		}
+	});
+	
+});
+
