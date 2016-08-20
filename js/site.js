e6e69299aa8ba3705995cee50f7f5f200f5ebdc5
Matt Brock <mbrock8@gmail.com>
2016-08-19 23:27:02 -0500

    get tabs style working in nav

--- js/site.js
+++ js/site.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
 	var pagePath = window.location.pathname;
 	
 	$('.nav__item').each(function(){
-		var h = $(this).find('a').attr('href').split('/')[1];
-		if(pagePath.search(h) > -1){
-			$(this).addClass('nav__item--active');
+		if(!$(this).hasClass('nav__item--spacer')){
+			var h = $(this).find('a').attr('href').split('/')[1];
+			if(pagePath.search(h) > -1){
+				$(this).addClass('nav__item--active');
+			}
 		}
 	});
 	
