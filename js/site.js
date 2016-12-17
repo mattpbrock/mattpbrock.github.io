e5c9b6e20e906d64baab39d445a639034368ee9c
Matt Brock <mbrock8@gmail.com>
2016-12-17 15:08:59 -0600

    add backdrop-filter to site intro because Apple is cool; bump border size up on nav links; mess around with unique header border top color for different pages

--- js/site.js
+++ js/site.js
@@ -2,15 +2,17 @@
 
 $(document).ready(function(){
 	
-	var pagePath = window.location.pathname;
+	const pagePath = window.location.pathname;
 	
 	$('.nav__item').each(function(){
 		if(!$(this).hasClass('nav__item--spacer')){
-			var h = $(this).find('a').attr('href').split('/')[1];
+			let h = $(this).find('a').attr('href').split('/')[1];
 			if(pagePath.search(h) > -1){
 				$(this).addClass('nav__item--active');
 			}
 		}
+		console.log(pagePath.split('/')[1]);
+		$('body').addClass('p--' + pagePath.split('/')[1]);
 	});
 	
 });
